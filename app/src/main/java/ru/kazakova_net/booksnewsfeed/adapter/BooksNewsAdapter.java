acb533a4fd251413a0e57f7660f5ea65b922776c
nkazakova <natalia.kazakova@innoway.ru>
2018-06-18 23:56:30 +0300

    Intermediate commit

--- app/src/main/java/ru/kazakova_net/booksnewsfeed/adapter/BooksNewsAdapter.java
+++ app/src/main/java/ru/kazakova_net/booksnewsfeed/adapter/BooksNewsAdapter.java
@@ -3,13 +3,17 @@ package ru.kazakova_net.booksnewsfeed.adapter;
 import android.app.Activity;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-import java.text.DecimalFormat;
+import com.squareup.picasso.Picasso;
+
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
@@ -22,6 +26,8 @@ import ru.kazakova_net.booksnewsfeed.model.BooksNews;
  * Created by Kazakova_net on 18.06.2018.
  */
 public class BooksNewsAdapter extends ArrayAdapter<BooksNews> {
+    private final static String LOG_TAG = BooksNewsAdapter.class.getSimpleName();
+    
     public BooksNewsAdapter(@NonNull Activity context, @NonNull List<BooksNews> booksNews) {
         super(context, 0, booksNews);
     }
@@ -30,49 +36,44 @@ public class BooksNewsAdapter extends ArrayAdapter<BooksNews> {
     @Override
     public View getView(final int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         View listItemView = convertView;
+        
         if (listItemView == null) {
             listItemView = LayoutInflater.from(getContext()).inflate(R.layout.books_news_list_item, parent, false);
         }
         
+        ImageView thumbnailImageView = listItemView.findViewById(R.id.thumbnail);
         TextView webPublicationDateTextView = listItemView.findViewById(R.id.web_publication_date);
         TextView webTitleTextView = listItemView.findViewById(R.id.web_title);
         TextView trailTextTextView = listItemView.findViewById(R.id.trail_text);
         
         BooksNews booksNews = getItem(position);
+        
         if (booksNews != null) {
+            Picasso.get().load(booksNews.getThumbnail()).into(thumbnailImageView);
             
             webTitleTextView.setText(booksNews.getWebTitle());
             trailTextTextView.setText(booksNews.getTrailText());
-            
-            String webPublicationDate = booksNews.getWebPublicationDate();
-            webPublicationDateTextView.setText(webPublicationDate);
+            webPublicationDateTextView.setText(formatDate(booksNews.getWebPublicationDate()));
         }
         
         return listItemView;
     }
     
     /**
-     * Return the formatted date string (i.e. "Mar 3, 1984") from a Date object.
+     * Return the formatted date string (i.e. "03.05.2018, 22:15") from a Date object.
      */
-    private String formatDate(Date dateObject) {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("LLL dd, yyyy", Locale.getDefault());
-        return dateFormat.format(dateObject);
-    }
-    
-    /**
-     * Return the formatted date string (i.e. "4:30 PM") from a Date object.
-     */
-    private String formatTime(Date dateObject) {
-        SimpleDateFormat timeFormat = new SimpleDateFormat("h:mm a", Locale.getDefault());
-        return timeFormat.format(dateObject);
-    }
-    
-    /**
-     * Return the formatted magnitude string showing 1 decimal place (i.e. "3.2")
-     * from a decimal magnitude value.
-     */
-    private String formatMagnitude(double magnitude) {
-        DecimalFormat formatter = new DecimalFormat("0.0");
-        return formatter.format(magnitude);
+    private String formatDate(String stringDateObject) {
+        SimpleDateFormat dateFormatInput = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.getDefault());
+        SimpleDateFormat dateFormatOutput = new SimpleDateFormat("dd.MM.yyyy, HH:mm", Locale.getDefault());
+        
+        Date date = new Date();
+        
+        try {
+            date = dateFormatInput.parse(stringDateObject);
+        } catch (ParseException e) {
+            Log.e(LOG_TAG, "Problem parsing the books news webPublicationDate", e);
+        }
+        
+        return dateFormatOutput.format(date);
     }
 }
