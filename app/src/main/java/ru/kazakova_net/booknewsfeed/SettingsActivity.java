e7980c701b72fb3feeb6b5f1552059d00d7fe397
nkazakova <natalia.kazakova@innoway.ru>
2018-08-02 15:26:38 +0300

    Validation of the user-entered value for setting page_size is added

--- app/src/main/java/ru/kazakova_net/booknewsfeed/SettingsActivity.java
+++ app/src/main/java/ru/kazakova_net/booknewsfeed/SettingsActivity.java
@@ -1,12 +1,18 @@
 package ru.kazakova_net.booknewsfeed;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputFilter;
+import android.text.Spanned;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class SettingsActivity extends AppCompatActivity {
@@ -23,8 +29,12 @@ public class SettingsActivity extends AppCompatActivity {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.settings_main);
             
-            Preference minMagnitude = findPreference(getString(R.string.settings_page_size_key));
-            bindPreferenceSummaryToValue(minMagnitude);
+            EditText pageSizeEditText = ((EditTextPreference) findPreference(getString(R.string.settings_page_size_key)))
+                    .getEditText();
+            pageSizeEditText.setFilters(new InputFilter[]{new InputFilterMinMax(getContext(), 1, 50)});
+            
+            Preference pageSize = findPreference(getString(R.string.settings_page_size_key));
+            bindPreferenceSummaryToValue(pageSize);
             
             Preference orderBy = findPreference(getString(R.string.settings_order_by_key));
             bindPreferenceSummaryToValue(orderBy);
@@ -58,6 +68,56 @@ public class SettingsActivity extends AppCompatActivity {
             String preferenceString = preferences.getString(preference.getKey(), "");
             
             onPreferenceChange(preference, preferenceString);
+        }
+    }
+    
+    private static class InputFilterMinMax implements InputFilter {
+        
+        private int mMin;
+        private int mMax;
+        private Context mContext;
+        
+        InputFilterMinMax(Context context, int min, int max) {
+            mContext = context;
+            mMin = min;
+            mMax = max;
+        }
+        
+        InputFilterMinMax(Context context, String min, String max) {
+            mContext = context;
+            mMin = Integer.parseInt(min);
+            mMax = Integer.parseInt(max);
+        }
+        
+        @Override
+        public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend) {
+            try {
+                int input = Integer.parseInt(dest.toString() + source.toString());
+                
+                if (isInRange(mMin, mMax, input)) {
+                    return null;
+                } else {
+                    Toast.makeText(mContext, "The value must be between " + mMin + " and " + mMax,
+                            Toast.LENGTH_SHORT).show();
+                }
+            } catch (NumberFormatException nfe) {
+                nfe.printStackTrace();
+            }
+            
+            return "";
+        }
+    
+        /**
+         * Returns whether the value entered by the user is in the specified interval
+         *
+         * @param a minimum allowable value
+         * @param b maximum allowable value
+         * @param c value entered by the user
+         * @return is value in range
+         */
+        
+        private boolean isInRange(int a, int b, int c) {
+            return b > a ? c >= a && c <= b : c >= b && c <= a;
         }
     }
 }
